67c673c25edc05cddd23214f79c300e9dcc9622c
Igor Goldny <igoldny@gmail.com>
2013-09-15 13:55:58 +0300

    wire all

---
+++ all/index.js
@@ -0,0 +1,24 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var fleck = require('fleck');
+
+var AllGenerator = module.exports = function AllGenerator(args, options, config) {
+  this.hookFor('ftbpro:presenter', {
+    args: args
+  });
+  this.hookFor('ftbpro:view', {
+    args: args
+  });
+  this.hookFor('ftbpro:template', {
+    args: args
+  });
+  this.hookFor('ftbpro:css', {
+    args: args
+  });
+  this.hookFor('ftbpro:spec', {
+    args: args
+  });
+};
+
+util.inherits(AllGenerator, yeoman.generators.NamedBase);
