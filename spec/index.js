a4bcc8f4c3764e5027cc7d2ee2bbff7fbc7f8f22
Igor Goldny <igoldny@gmail.com>
2013-09-15 16:43:58 +0300

    fix spec path

--- spec/index.js
+++ spec/index.js
@@ -14,19 +14,19 @@ util.inherits(SpecGenerator, yeoman.generators.NamedBase);
 
 SpecGenerator.prototype.files = function files() {
   if (this.options.collections) {
-    this.copy('spec_model.js.coffee', 'spec/singlepage/models/' + this.underscored_name + '_spec.js.coffee');
-    this.copy('spec_collection.js.coffee', 'spec/singlepage/collections/' + this.underscored_name + '_spec.js.coffee');
-    return this.copy('spec_view_model_collections.js.coffee', 'spec/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
+    this.copy('spec_model.js.coffee', 'spec/javascripts/singlepage/models/' + this.underscored_name + '_spec.js.coffee');
+    this.copy('spec_collection.js.coffee', 'spec/javascripts/singlepage/collections/' + this.underscored_name + '_spec.js.coffee');
+    return this.copy('spec_view_model_collections.js.coffee', 'spec/javascripts/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
   }
 
   if (this.options.model) {
-    this.copy('spec_model.js.coffee', 'spec/singlepage/models/' + this.underscored_name + '_spec.js.coffee');
-    return this.copy('spec_view_model.js.coffee', 'spec/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
+    this.copy('spec_model.js.coffee', 'spec/javascripts/singlepage/models/' + this.underscored_name + '_spec.js.coffee');
+    return this.copy('spec_view_model.js.coffee', 'spec/javascripts/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
   }
 
   if (this.options.hash) {
-    return this.copy('spec_view_hash.js.coffee', 'spec/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
+    return this.copy('spec_view_hash.js.coffee', 'spec/javascripts/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
   }
 
-  this.copy('spec_view.js.coffee', 'spec/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
+  this.copy('spec_view.js.coffee', 'spec/javascripts/singlepage/views/' + this.underscored_name + '_spec.js.coffee');
 };
